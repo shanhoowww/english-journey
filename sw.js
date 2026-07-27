ce7ad9f5cf365de8f53f7229466eeb99bc5fe71b
shanhoowww <shanhoo.wei@gmail.com>
2026-07-27 22:42:18 +0800

    Publish English Journey web app

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = "english-journey-v4";
+const BASE = new URL("./", self.location.href).pathname;
+const asset = (name = "") => `${BASE}${name}`;
+const APP_SHELL = [asset(), asset("manifest.webmanifest"), asset("journey-version.json"), asset("icon-192.png"), asset("icon-512.png"), asset("apple-touch-icon.png")];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(asset(), copy));
+      return response;
+    }).catch(() => caches.match(asset())));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  })));
+});
